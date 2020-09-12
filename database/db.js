5013094cd3aa1b3e1dd6d89c6dadb242376186fb
shubhams16 <shubhamsingh200@gmail.com>
2020-09-12 10:44:15 +0530

    changes in port 8080

--- database/db.js
+++ database/db.js
@@ -1,7 +1,7 @@
 const Sequelize = require("sequelize")
 const  db= {}
-const sequelize = new Sequelize("tapdashreact", "root", "", {
-    host: "localhost",
+const sequelize = new Sequelize("node_dashbard_rds", "admin", "TapNode2020WltRe", {
+    host: "tap-dash-node-db.csgvgsfkqcga.eu-west-2.rds.amazonaws.com",
     dialect: 'mysql',
     operatorsAliases: false,
 

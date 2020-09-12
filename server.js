387b8a20f065cf5b8445621b5b90f1bd6fb56c43
shubhams16 <shubhamsingh200@gmail.com>
2020-09-12 10:46:38 +0530

    Add Default route

--- server.js
+++ server.js
@@ -9,6 +9,10 @@ app.use(bodyParser.json())
 app.use(cors())
 app.use(bodyParser.urlencoded({extended: false}))
 
+app.get('/', function(req, res){
+    res.send('Hello Shubham');
+  });
+
 var Users =require('./routes/Users')
 app.use('/users', Users)
 
